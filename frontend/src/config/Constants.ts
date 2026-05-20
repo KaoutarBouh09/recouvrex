ab0306dfb2683218ad114e92b180dfc0c148b5bb
KaoutarBouh09 <kaoutarbouh82@gmail.com>
2026-05-20 20:44:51 +0100

    feat: ArgoCD application setup

--- frontend/src/config/Constants.ts
+++ frontend/src/config/Constants.ts
@@ -10,7 +10,7 @@ const dev = {
 const prod = {
   url: {
     KEYCLOAK_BASE_URL: "http://localhost:8080",
-    API_BASE_URL: '/api',
+    API_BASE_URL: '',
     OMDB_BASE_URL: 'https://www.omdbapi.com',
     AVATARS_DICEBEAR_URL: 'https://api.dicebear.com/6.x'
   }
